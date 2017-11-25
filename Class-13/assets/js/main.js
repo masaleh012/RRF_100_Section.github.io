d59ecdd5f1022d9968daa48dfc3088bd99e96f73
MD. ABU SALEH <32726631+masaleh012@users.noreply.github.com>
2017-11-25 15:17:32 +0600

    Class-13
    Class-13

---
+++ Class-13/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(".products-list").masonry();
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
