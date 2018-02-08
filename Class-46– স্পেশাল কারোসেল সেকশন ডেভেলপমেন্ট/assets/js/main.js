9630def6d4a25704d2da5ce94bf6acfbef918595
MD. ABU SALEH <32726631+masaleh012@users.noreply.github.com>
2018-02-08 15:49:01 +0600

    Class-46

---
+++ Class-46– স্পেশাল কারোসেল সেকশন ডেভেলপমেন্ট/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        $(".featured-carousel-box").owlCarousel({
+            items: 1,
+            autoplay: false,
+            loop: true,
+            dots: true,
+            nav: false
+        });
+
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
