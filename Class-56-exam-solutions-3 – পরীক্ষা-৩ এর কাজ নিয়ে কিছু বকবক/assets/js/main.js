fa9ea2a0b6f220b911d101acab5d25e6e7cd9090
MD. ABU SALEH <32726631+masaleh012@users.noreply.github.com>
2018-02-15 20:03:50 +0600

    Class-56

---
+++ Class-56-exam-solutions-3 – পরীক্ষা-৩ এর কাজ নিয়ে কিছু বকবক/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+        $(".theme-1-select").on('click', function () {
+            $("body").addClass("theme-1").removeClass("theme-2");
+            return false;
+        });
+        $(".theme-2-select").on('click', function () {
+            $("body").addClass("theme-2").removeClass("theme-1");
+            $("html").css("height", "100%");
+            return false;
+        });
+
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
