288a34563af45dcc3fd13781a7922133646d599e
MD. ABU SALEH <32726631+masaleh012@users.noreply.github.com>
2018-03-08 16:04:16 +0600

    Class-67

---
+++ Class-67 – % হিরো এরিয়া ট্রিকস/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(".mainmenu li a").addClass("active");
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function () {
+
+
+    });
+
+
+}(jQuery));
